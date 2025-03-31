bec7e8316ad0e2708f77ac8dd3e4b6402f1b05af
Riday <rdmondal100@gmail.com>
2025-03-31 15:15:08 +0600

    revert: previous url

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -8,13 +8,13 @@ export function cn(...inputs) {
 }
 
 
-export const socket = io('https://mern-quick-chat-app-server.vercel.app/')
+export const socket = io('https://mern-quick-chat-app-server.vercel.app')
 
 export const currentTime = () => new Date().toISOString().replace('Z', '+00:00');
 
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://mern-quick-chat-app-server.vercel.app/',
+  baseURL: 'https://mern-quick-chat-app-server.vercel.app',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
